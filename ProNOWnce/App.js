5d0b93666b92d93bef7f8804a79f2bc7181a8d1c
Zui <zuikng@gmail.com>
2017-11-04 05:28:06 -0400

    Connect the front-end with the basic API

--- ProNOWnce/App.js
+++ ProNOWnce/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { AppRegistry,StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+
+const ENDPOINT = "http://72.19.107.126:5000/word";
 
 
 // function randoWord {
@@ -10,26 +12,52 @@ import { AppRegistry,StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
 
-  randoWord(){ //for when we have the words to pronounce
-    var words = ["word", "curse", "jagged"]; //TODO: Change the list to format to [[word1, sfx1,sfx2],[word2, sfx1,sfx2]]
-    var chose = words[Math.floor(Math.random()*words.length)];
-    return chose;
-  }
+    constructor(props, context) {
+        super(props, context);
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>{this.randoWord()}</Text>
-      </View>
-    );
-  }
+        this.state = {
+            word: ""
+        }
+    }
+
+
+    async getWords() {
+        try {
+            let res = await fetch(`${ENDPOINT}`);
+            let resJSON = await res.json();
+
+            this.setState({
+                word: resJSON.word
+            });
+            return resJSON.word;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    componentDidMount() {
+        this.getWords();
+    }
+
+    // randomWord(){ //for when we have the words to pronounce
+    //   var words = ["word", "curse", "jagged"]; //TODO: Change the list to format to [[word1, sfx1,sfx2],[word2,
+    // sfx1,sfx2]] var chose = words[Math.floor(Math.random()*words.length)]; return chose; }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>{this.state.word}</Text>
+                <Text>More work needed to be done here!</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
