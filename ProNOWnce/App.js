b62acb0d1e634ad377fc1b5e65de006fc62d2c56
Ashley Deshields <ashley.deshields.2016@gmail.com>
2017-11-04 09:50:12 -0400

    em commit

--- ProNOWnce/App.js
+++ ProNOWnce/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 const ENDPOINT = "http://72.19.107.126:5000/word";
-
-
-// function randoWord {
-//   var words = ["word", "curse", "jagged"];
-//   var chose = words[Math.floor(Math.random()*words.length)];
-//   return chose;
-//   }
 
 export default class App extends React.Component {
 
@@ -19,7 +12,6 @@ export default class App extends React.Component {
             word: ""
         }
     }
-
 
     async getWords() {
         try {
@@ -39,18 +31,20 @@ export default class App extends React.Component {
         this.getWords();
     }
 
+
     // randomWord(){ //for when we have the words to pronounce
-    //   var words = ["word", "curse", "jagged"]; //TODO: Change the list to format to [[word1, sfx1,sfx2],[word2,
-    // sfx1,sfx2]] var chose = words[Math.floor(Math.random()*words.length)]; return chose; }
+    //   var words = ["word", "curse", "jagged", "touch", "mouse"]; //TODO: Change the list to format to [[word1, sfx1,sfx2],[word2,
+    //   //sfx1,sfx2]]
+    //   var chose = words[Math.floor(Math.random()*words.length)]; return chose; }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.title}>Pick one of these options.</Text>
+                <Text style={styles.title}>{this.state.word}</Text>
                 <View style={styles.buttons}>
                     <View style={styles.leftColumn}>
                         <Button
-                            onPress={() => { Alert.alert('Right Answer.') }}
+                            onPress={() => { Alert.alert('Right Answer.') }} //
                             title="Option 1"
                         />
                         <Button
