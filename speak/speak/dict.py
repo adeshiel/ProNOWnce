964fa3804d6185ee54e391224cca7df832647f98
Zui <zuikng@gmail.com>
2017-11-04 14:04:44 -0400

    Random vowel replacement

--- speak/speak/dict.py
+++ speak/speak/dict.py
@@ -1,9 +1,44 @@
 from pprint import pprint
 
 from pywiktionary import Wiktionary
+import random
 
 wikt = Wiktionary(XSAMPA=True)
 
+
+def load_wordlist(path):
+    """
+    Load wordlist by path
+
+    :param path:
+    :return:
+    """
+
+    with open(path, "rb") as f:
+        r = f.read()
+        r = r.decode(encoding="utf-8")
+    return r.split("\n")
+
+
+WL = load_wordlist("wordlist.txt")
+
+
+def random_word():
+    """
+    Pick a random word
+
+    :return:
+    """
+
+    ix = random.randint(0, len(WL))
+
+    return WL[ix]
+
+
+
+
 if __name__ == '__main__':
-    word = wikt.lookup("dictionary")
-    pprint(word)
+    # word = wikt.lookup("dictionary")
+    # pprint(word)
+    for i in range(10):
+        print(random_word())
--- speak/speak/speech.py
+++ speak/speak/speech.py
@@ -3,6 +3,7 @@ Dealing with Amazon's speech synthesis API
 """
 import logging
 
+import random
 from boto3 import Session
 from botocore.exceptions import BotoCoreError, ClientError
 from contextlib import closing
@@ -27,6 +28,8 @@ def polly_req(ssml):
 
     try:
         # Request speech synthesis
+        l.debug("Requesting polly for %s", ssml)
+
         response = polly.synthesize_speech(TextType="ssml",
                                            Text=ssml,
                                            OutputFormat="mp3",
@@ -36,6 +39,29 @@ def polly_req(ssml):
         # The service returned an error, exit gracefully
         l.error(error)
         return
+
+
+def save_pron(res, filename):
+    """
+    Save the audiostream to file
+
+    :param res:
+    :param filename:
+    :return: path to the file saved
+    """
+    with closing(res["AudioStream"]) as stream:
+        output = os.path.join(os.getcwd(), "prons", "{filename}.mp3".format(filename=filename))
+        l.debug("Saving %s", filename)
+        l.debug("Data %s", res)
+        try:
+            # Open a file for writing the output as a binary stream
+            with open(output, "wb") as file:
+                file.write(stream.read())
+                return output
+        except IOError as error:
+            # Could not write to file, exit gracefully
+            l.error("Can't write file %s", filename)
+            sys.exit(-1)
 
 
 class Word:
@@ -49,4 +75,53 @@ class Word:
 
         :param str word: a word in English
         """
-        pass
+        self.word = word
+        self.path = []
+
+    def speak(self):
+        """
+        Generate a pronunciation
+
+        :return:
+        """
+        res = polly_req(f"<speak>{self.word}</speak>")
+        self.path.append(save_pron(res, filename=self.word))
+        return self.path
+
+    def messup(self):
+        """
+        Generate three weird speech pattern
+
+        :return:
+        """
+
+        res = polly_req(f"<speak>{vowel_rem(self.word)}</speak>")
+        self.path.append(save_pron(res, filename=self.word + "-a"))
+
+        res = polly_req(f"<speak>{vowel_rem(self.word)}</speak>")
+        self.path.append(save_pron(res, filename=self.word + "-b"))
+
+        res = polly_req(f"<speak>{vowel_rem(self.word)}</speak>")
+        self.path.append(save_pron(res, filename=self.word + "-c"))
+
+        return self.path
+
+
+def vowel_rem(word):
+    """
+    Replace vowels in the word
+
+    :param word:
+    :return:
+    """
+    r = ""
+    for c in word:
+        if c in "aeiou":
+            r += random.choice("aeiou".replace(c, ""))
+        else:
+            r += c
+    return r
+
+
+if __name__ == '__main__':
+    print(vowel_rem("antidistablishmentarianism"))
